1c1fa5e670cf9b4377c8aab5cf6b0e7d0bafc4c3
Paulo2991 <paulovaz38034@gmail.com>
2021-02-27 09:39:13 -0300

    Fazendo o projeto funcionar

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,5 +1,8 @@
+import {useState} from 'react';
 import styles from '../styles/components/Countdown.module.css';
 export function Countdown(){
+    const[time,setTime] = useState(25 *60);
+    const minutes = time /60;
     return(
         <div className={styles.CountdownContainer}>
             <div>
